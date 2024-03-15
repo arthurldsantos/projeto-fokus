bb0b71c0246e5a10494df7219e02ea691329f23f
Arthur Santos <arthurldsantos@gmail.com>
2024-03-15 17:31:17 -0300

    adicionando a classe ativa na tarefa

--- script-crud.js
+++ script-crud.js
@@ -3,6 +3,7 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const textarea = document.querySelector('.app__form-textarea');
 const ulTarefas = document.querySelector('.app__section-task-list');
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
@@ -35,9 +36,9 @@ function criarElementoTarefa(tarefa) {
     botao.classList.add('app_button-edit');
 
     botao.onclick = () => {
-        debugger
+        // debugger
         const novaDescricao = prompt('Qual é o novo nome da tarefa?');
-        console.log('Nova descrição da tarefa: ', novaDescricao);
+        // console.log('Nova descrição da tarefa: ', novaDescricao);
         if(novaDescricao) {
             paragrafo.textContent = novaDescricao;
             tarefa.descricao = novaDescricao;
@@ -52,6 +53,11 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    li.onclick = () => {
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+        li.classList.add('app__section-task-list-item-active');
+    }
 
     return li;
 }
