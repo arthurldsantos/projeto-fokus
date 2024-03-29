cd38571e98be69b76111429e3025cfbe91dc49fe
Arthur Santos <arthurldsantos@gmail.com>
2024-03-29 16:05:25 -0300

    criando evento customizado para finalizar tarefa

--- script-crud.js
+++ script-crud.js
@@ -7,6 +7,7 @@ const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-ta
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefas() {
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
@@ -62,9 +63,11 @@ function criarElementoTarefa(tarefa) {
         if (tarefaSelecionada == tarefa) {
             paragrafoDescricaoTarefa.textContent = '';
             tarefaSelecionada = null;
+            liTarefaSelecionada = null;
             return;
         }
         tarefaSelecionada = tarefa;
+        liTarefaSelecionada = li;
         paragrafoDescricaoTarefa.textContent = tarefa.descricao;
         li.classList.add('app__section-task-list-item-active');
     }
@@ -98,21 +101,10 @@ btnCancelar.addEventListener('click', () => {
     limparConteudoTarefa();
 });
 
-/*
-
-<li class="app__section-task-list-item">
-    <svg>
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
-    </svg>
-    <p class="app__section-task-list-item-description">
-        Estudando localStorage
-    </p>
-    <button class="app_button-edit">
-        <img src="/imagens/edit.png">
-    </button>
-</li>
-
-*/
+document.addEventListener('FocoFinalizado', () => {
+    if(tarefaSelecionada && liTarefaSelecionada) {
+        liTarefaSelecionada.classList.remove('app__section-task-list-item-active');
+        liTarefaSelecionada.classList.add('app__section-task-list-item-complete');
+        liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled');
+    }
+});
--- script.js
+++ script.js
@@ -17,7 +17,8 @@ const encerraTemporizador = new Audio('/sons/beep.mp3');
 
 musica.loop = true;
 
-let tempoDecorridoEmSegundos = 1500;
+// let tempoDecorridoEmSegundos = 1500;
+let tempoDecorridoEmSegundos = 30;
 let intervaloId = null;
 
 function alterarContexto(contexto) {
@@ -56,19 +57,22 @@ function alterarContexto(contexto) {
 }
 
 focoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 1500;
+    // tempoDecorridoEmSegundos = 1500;
+    tempoDecorridoEmSegundos = 30;
     alterarContexto('foco');
     focoBt.classList.add('active');
 })
 
 curtoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 300;
+    // tempoDecorridoEmSegundos = 300;
+    tempoDecorridoEmSegundos = 5;
     alterarContexto('descanso-curto');
     curtoBt.classList.add('active');
 })
 
 longoBt.addEventListener('click', () => {
-    tempoDecorridoEmSegundos = 900;
+    // tempoDecorridoEmSegundos = 900;
+    tempoDecorridoEmSegundos = 15;
     alterarContexto('descanso-longo');
     longoBt.classList.add('active');
 })
@@ -85,6 +89,11 @@ const contagemRegressiva = () => {
     if(tempoDecorridoEmSegundos <= 0) {
         encerraTemporizador.play();
         alert('Temporizador finalizado!');
+        const focoAtivo = html.getAttribute('data-contexto') == 'foco';
+        if(focoAtivo) {
+            const evento = new CustomEvent('FocoFinalizado');
+            document.dispatchEvent(evento);
+        }
         zerar();
         return;
     }
